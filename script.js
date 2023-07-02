605fd1b07a283fc7348462f0628c4f015bafe561
mehul-0 <116076306+mehul-0@users.noreply.github.com>
2023-07-02 11:40:28 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,42 @@
-
+    const input = document.getElementById('box');
+    const list = document.getElementById('list');
+  
+    function addTask() {
+      if (input.value === '') {
+        alert("Please Enter some Task.");
+      } else {
+        let li = document.createElement('li');
+        li.innerHTML = input.value;
+        list.appendChild(li);
+        let span = document.createElement('span');
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
+      }
+      input.value = '';
+      save();
+    }
+    
+    list.addEventListener("click", function(e) {
+      if (e.target.tagName === 'LI') {
+        e.target.classList.toggle('checked');
+        save();
+      } else if (e.target.tagName === 'SPAN') {
+        e.target.parentElement.remove();
+        save();
+      }
+    }, false);
+    
+    function save() {
+      localStorage.setItem("data", list.innerHTML);
+    }
+    
+    function showTask() {
+      list.innerHTML = localStorage.getItem("data");
+    }
+    document.addEventListener("keydown", function(event) {
+        if (event.key === "Enter") {
+            addTask();
+        }
+      });
+    showTask();
+  
